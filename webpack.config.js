cc3a773967ab9d61073346308c35132288beef2b
tanhao <tanhao_282700@163.com>
2017-03-10 22:35:09 +0800

    添加config文件

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+
+'use strict';
+
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");  //css单独打包
+var htmlWebpackPlugin=require("html-webpack-plugin"); //html文件打包
+
+module.exports = {
+    devtool: 'eval-source-map',
+
+    entry: __dirname + '/src/entry.js', //唯一入口文件
+    output: {
+        path: __dirname + '/build', //打包后的文件存放的地方
+        filename: 'bundle.js' //打包后输出文件的文件名
+    },
+    /*externals: {
+        jquery: 'window.$'  //全局配置jq
+    },*/
+
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: "jsx!babel", include: /src/},
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style", "css!postcss")},
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css!postcss!sass")},
+            { test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
+                /*loaders: [
+                    'url-loader?limit=8192',
+                    'image-webpack'
+                ]*/
+            }
+        ]
+    },
+
+    postcss: [
+        require('autoprefixer')    //调用autoprefixer插件,css3自动补全
+    ],
+
+    devServer: {
+        // contentBase: './src/views'  //本地服务器所加载的页面所在的目录
+        port: 8888,
+        colors: true,  //终端中输出结果为彩色
+        historyApiFallback: true,  //不跳转
+        inline: true  //实时刷新
+    },
+
+    plugins: [
+        new ExtractTextPlugin('main.css'),
+        new htmlWebpackPlugin({
+            filename:'main.html',
+            template: 'index.html',
+            inject: 'body'
+        })
+    ]
+
+}
