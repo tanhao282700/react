ddb8cad532752a9955154788f10b95614e8915c3
company tanhao <542443538@qq.com>
2017-03-13 13:37:38 +0800

    路由配置

--- src/components/App.js
+++ src/components/App.js
@@ -12,29 +12,6 @@ const SubMenu = Menu.SubMenu;
 
 
 
-/*var App = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <img src={require('../images/yeoman.png')} />
-                <h5 className="title">hello, yeoman app!</h5>
-                <div>React Router: </div>
-                <div><a href="#/first">first page</a></div>
-                <div><a href="#/second">second page</a></div>
-            </div>
-        );
-    }
-});
-
-//最终渲染
-ReactDom.render((
-    <Router history={hashHistory}>
-        <Route path='/' component={App}></Route>
-        <Route path='/first' component={Firsts} />
-        <Route path='/second' component={Seconds} />
-    </Router>
-), document.getElementById('app'));*/
-
 class Sider extends React.Component {
     state = {
         current: '1',
@@ -75,61 +52,61 @@ class Sider extends React.Component {
                 onClick={this.handleClick}
             >
                 <SubMenu key="sub1" title={<span><Icon type="mail" /><span>用户列表</span></span>}>
-                    <Menu.Item key="1">账号列表</Menu.Item>
-                    <Menu.Item key="2">限制列表</Menu.Item>
+                    <Menu.Item key="1"><a href="#/users/index">账号管理</a></Menu.Item>
+                    <Menu.Item key="2"><a href="#/users/limit/limit">限制列表</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>群运营</span></span>}>
-                    <Menu.Item key="3">群列表</Menu.Item>
-                    <Menu.Item key="4">群分类</Menu.Item>
+                    <Menu.Item key="3"><a href="#/circle/circleList">群列表</a></Menu.Item>
+                    <Menu.Item key="4"><a href="#">群分类</a></Menu.Item>
                     {/*<SubMenu key="sub3" title="Submenu">
                         <Menu.Item key="7">Option 7</Menu.Item>
                         <Menu.Item key="8">Option 8</Menu.Item>
                     </SubMenu>*/}
                 </SubMenu>
                 <SubMenu key="sub4" title={<span><Icon type="setting" /><span>说说管理</span></span>}>
-                    <Menu.Item key="5">说说列表</Menu.Item>
+                    <Menu.Item key="5"><a href="#/dynamic/index">说说列表</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub5" title={<span><Icon type="setting" /><span>日记管理</span></span>}>
-                    <Menu.Item key="6">日记列表</Menu.Item>
-                    <Menu.Item key="7">日记本管理</Menu.Item>
+                    <Menu.Item key="6"><a href="#/dairy/dairyList">日记列表</a></Menu.Item>
+                    <Menu.Item key="7"><a href="#">日记本管理</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub6" title={<span><Icon type="setting" /><span>商品管理</span></span>}>
-                    <Menu.Item key="8">商品列表</Menu.Item>
-                    <Menu.Item key="9">添加商品</Menu.Item>
+                    <Menu.Item key="8"><a href="#/product/productList">商品列表</a></Menu.Item>
+                    <Menu.Item key="9"><a href="#/product/addProduct">添加商品</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub7" title={<span><Icon type="setting" /><span>机器人管理</span></span>}>
-                    <Menu.Item key="10">机器人设置</Menu.Item>
-                    <Menu.Item key="11">说说库</Menu.Item>
-                    <Menu.Item key="12">评论库</Menu.Item>
+                    <Menu.Item key="10"><a href="#/robot/setting">机器人设置</a></Menu.Item>
+                    <Menu.Item key="11"><a href="#/robot/dynamicLibrary">说说库</a></Menu.Item>
+                    <Menu.Item key="12"><a href="#/robot/comments">评论库</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub8" title={<span><Icon type="setting" /><span>举报管理</span></span>}>
-                    <Menu.Item key="13">举报列表</Menu.Item>
+                    <Menu.Item key="13"><a href="#/report/index">举报列表</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub9" title={<span><Icon type="setting" /><span>帮助反馈</span></span>}>
-                    <Menu.Item key="14">帮助中心</Menu.Item>
-                    <Menu.Item key="15">意见反馈</Menu.Item>
+                    <Menu.Item key="14"><a href="#/feedback/help">帮助中心</a></Menu.Item>
+                    <Menu.Item key="15"><a href="#/feedback/feedback"></a>意见反馈</Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub10" title={<span><Icon type="setting" /><span>资金管理</span></span>}>
-                    <Menu.Item key="16">交易记录</Menu.Item>
-                    <Menu.Item key="17">充值记录</Menu.Item>
-                    <Menu.Item key="18">提现记录</Menu.Item>
-                    <Menu.Item key="19">红包管理</Menu.Item>
-                    <Menu.Item key="20">打赏管理</Menu.Item>
-                    <Menu.Item key="21">金币管理</Menu.Item>
+                    <Menu.Item key="16"><a href="#">交易记录</a></Menu.Item>
+                    <Menu.Item key="17"><a href="#">充值记录</a></Menu.Item>
+                    <Menu.Item key="18"><a href="#">提现记录</a></Menu.Item>
+                    <Menu.Item key="19"><a href="#">红包管理</a></Menu.Item>
+                    <Menu.Item key="20"><a href="#">打赏管理</a></Menu.Item>
+                    <Menu.Item key="21"><a href="#">金币管理</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub11" title={<span><Icon type="setting" /><span>推荐管理</span></span>}>
-                    <Menu.Item key="22">附近榜单</Menu.Item>
-                    <Menu.Item key="23">发现推荐</Menu.Item>
-                    <Menu.Item key="24">背景推荐</Menu.Item>
-                    <Menu.Item key="25">热词管理</Menu.Item>
-                    <Menu.Item key="26">banner推荐</Menu.Item>
+                    <Menu.Item key="22"><a href="#">附近榜单</a></Menu.Item>
+                    <Menu.Item key="23"><a href="#">发现推荐</a></Menu.Item>
+                    <Menu.Item key="24"><a href="#">背景推荐</a></Menu.Item>
+                    <Menu.Item key="25"><a href="#">热词管理</a></Menu.Item>
+                    <Menu.Item key="26"><a href="#">banner推荐</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub12" title={<span><Icon type="setting" /><span>版本管理</span></span>}>
-                    <Menu.Item key="27">版本列表</Menu.Item>
+                    <Menu.Item key="27"><a href="#">版本列表</a></Menu.Item>
                 </SubMenu>
                 <SubMenu key="sub13" title={<span><Icon type="setting" /><span>权限管理</span></span>}>
-                    <Menu.Item key="28">管理员</Menu.Item>
-                    <Menu.Item key="29">功能权限</Menu.Item>
+                    <Menu.Item key="28"><a href="#">管理员</a></Menu.Item>
+                    <Menu.Item key="29"><a href="#">功能权限</a></Menu.Item>
                 </SubMenu>
             </Menu>
         );
@@ -156,35 +133,25 @@ var App = React.createClass({
                         <div className="nickName col-lg-7"><a href="#">梅梅</a></div>
                     </div>
                 </div>
-                {/*<div className="app-nav">
-                    <ul>
-                        <li>
-                            <a>用户管理</a>
-                            <ul>
-                                <li><a href="#/users/index">账号管理</a></li>
-                                <li><a href="#/users/limit/limit">账号列表</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a>群运营</a>
-                            <ul>
-                                <li><a>群列表</a></li>
-                                <li><a>群分类</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <div className="view">
-                    <Router history={hashHistory}>
-                        <Route path='/' component={Users} />
-                        <Route path='/users/index' component={Users} />
-                        <Route path='/users/limit/limit' component={Limit} />
-                    </Router>
-                </div>*/}
                 <div className="app-contain">
                     <div className="row app-body">
                         <div className="app-nav col-lg-2">
                             <Sider />
+                        </div>
+                        <div className="col-lg-10 app-view" classID="view">
+                            <Router history={hashHistory}>
+                                <Route path='/' component={Users} />
+                                <Route path='/users/index' component={Users} />
+                                <Route path='/users/limit/limit' component={Limit} />
+                                <Route path='/circle/circleList' component={Limit} />
+                                <Route path='/dynamic/index' component={Limit} />
+                                <Route path='/dairy/dairyList' component={Limit} />
+                                <Route path='/product/productList' component={Limit} />
+                                <Route path='/product/addProduct' component={Limit} />
+                                <Route path='/robot/setting' component={Limit} />
+                                <Route path='/robot/dynamicLibrary' component={Limit} />
+                                <Route path='/robot/comments' component={Limit} />
+                            </Router>
                         </div>
                     </div>
                 </div>
